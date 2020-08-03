9770014259756e25472bbd581f2c8a80e1bf3f28
Er prince kumar ojha <47948908+Erprincekumar@users.noreply.github.com>
2020-08-03 18:33:45 +0530

    one more Question

---
+++ src/ReverseElementInArray.kt
@@ -0,0 +1,24 @@
+//8.Write a function to reverese all the element in array
+
+fun<T> swap(arr: Array<T?>, i: Int, j: Int) {
+    val temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+}
+
+fun<T> reverse(arr: Array<T?>) {
+    var low = 0
+    var high = arr.size - 1
+    while (low < high) {
+        swap(arr, low, high)
+        low++
+        high--
+    }
+}
+
+fun main() {
+    val arr: Array<Int?> = arrayOf(1, 2, 3, 4, 5)
+    reverse(arr)
+
+    println(arr.contentToString())
+}
